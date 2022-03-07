efea85b6c04e48c8aae1a89ab3be9cba725d3401
AnthonyLzq <sluzquinosa@uni.pe>
2022-03-06 21:42:39 -0500

    feat: updated private fields in fastify

--- example/fastify/src/network/server.ts
+++ example/fastify/src/network/server.ts
@@ -6,16 +6,16 @@ import { applyRoutes } from './router'
 const PORT = process.env.PORT ?? '1996'
 
 class Server {
-  private _app: FastifyInstance
-  private _connection: mongoose.Connection | undefined
+  #app: FastifyInstance
+  #connection: mongoose.Connection | undefined
 
   constructor() {
-    this._app = Fastify({ logger: true })
-    this._config()
+    this.#app = Fastify({ logger: true })
+    this.#config()
   }
 
-  private _config() {
-    this._app.addHook('preHandler', (req, reply, done) => {
+  #config() {
+    this.#app.addHook('preHandler', (req, reply, done) => {
       reply.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
       reply.header('Access-Control-Allow-Origin', '*')
       reply.header(
@@ -24,25 +24,25 @@ class Server {
       )
       done()
     })
-    applyRoutes(this._app)
+    applyRoutes(this.#app)
   }
 
-  private async _mongo(): Promise<void> {
-    this._connection = mongoose.connection
+  async #mongo(): Promise<void> {
+    this.#connection = mongoose.connection
     const connection = {
       keepAlive: true,
       useNewUrlParser: true,
       useUnifiedTopology: true
     }
-    this._connection.on('connected', () => {
-      this._app.log.info('Mongo connection established.')
+    this.#connection.on('connected', () => {
+      this.#app.log.info('Mongo connection established.')
     })
-    this._connection.on('reconnected', () => {
-      this._app.log.info('Mongo connection reestablished')
+    this.#connection.on('reconnected', () => {
+      this.#app.log.info('Mongo connection reestablished')
     })
-    this._connection.on('disconnected', () => {
-      this._app.log.info('Mongo connection disconnected')
-      this._app.log.info('Trying to reconnected to Mongo...')
+    this.#connection.on('disconnected', () => {
+      this.#app.log.info('Mongo connection disconnected')
+      this.#app.log.info('Trying to reconnected to Mongo...')
       setTimeout(() => {
         mongoose.connect(process.env.MONGO_URI as string, {
           ...connection,
@@ -51,20 +51,20 @@ class Server {
         })
       }, 3000)
     })
-    this._connection.on('close', () => {
-      this._app.log.info('Mongo connection closed')
+    this.#connection.on('close', () => {
+      this.#app.log.info('Mongo connection closed')
     })
-    this._connection.on('error', (e: Error) => {
-      this._app.log.info('Mongo connection error:')
-      this._app.log.error(e)
+    this.#connection.on('error', (e: Error) => {
+      this.#app.log.info('Mongo connection error:')
+      this.#app.log.error(e)
     })
     await mongoose.connect(process.env.MONGO_URI as string, connection)
   }
 
   public async start(): Promise<void> {
     try {
-      await this._app.listen(PORT)
-      this._mongo()
+      await this.#app.listen(PORT)
+      this.#mongo()
     } catch (e) {
       console.error(e)
     }
