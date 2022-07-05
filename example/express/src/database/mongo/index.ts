e1f8b3292b0993027f7809a9b20de5758de1d6fe
AnthonyLzq <sluzquinosa@uni.pe>
2022-07-05 17:15:46 -0500

    feat: implemented tests and gha for express-rest case

--- example/express/src/database/mongo/index.ts
+++ example/express/src/database/mongo/index.ts
@@ -1,2 +1,3 @@
 export * from './models'
 export * from './queries'
+export * from './connection'
---
+++ example/fastify/src/database/mongo/connection.ts
@@ -0,0 +1,55 @@
+import { connect, connection } from 'mongoose'
+import { FastifyLoggerInstance } from 'fastify'
+
+const ENVIRONMENTS_WITHOUT_RECONNECTION = ['ci', 'local']
+const dbConnection = async (
+  logger: FastifyLoggerInstance
+): Promise<{
+  connect: () => Promise<typeof import('mongoose')>
+  disconnect: () => Promise<void>
+}> => {
+  const connectionConfig = {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+
+  connection.on('connected', () => {
+    logger.info('Mongo connection established.')
+  })
+  connection.on('reconnected', () => {
+    logger.info('Mongo connection reestablished')
+  })
+  connection.on('disconnected', () => {
+    if (
+      !ENVIRONMENTS_WITHOUT_RECONNECTION.includes(
+        process.env.NODE_ENV as string
+      )
+    ) {
+      logger.info(
+        'Mongo connection disconnected. Trying to reconnected to Mongo...'
+      )
+      setTimeout(() => {
+        connect(process.env.MONGO_URI as string, {
+          ...connection,
+          connectTimeoutMS: 3000,
+          socketTimeoutMS: 3000
+        })
+      }, 3000)
+    }
+  })
+  connection.on('close', () => {
+    logger.info('Mongo connection closed')
+  })
+  connection.on('error', (e: Error) => {
+    logger.info('Mongo connection error:')
+    logger.error(e)
+  })
+
+  return {
+    connect: () => connect(process.env.MONGO_URI as string, connectionConfig),
+    disconnect: () => connection.close()
+  }
+}
+
+export { dbConnection }
